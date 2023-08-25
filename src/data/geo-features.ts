56f453925e33412de2f6591576acfbe6a8357d5d
Abhishek SAINI <saini.abhishek@hotmail.com>
2023-08-25 12:55:50 +0200

    feat: data mock

--- src/data/geo-features.ts
+++ src/data/geo-features.ts
@@ -1,4 +1,4 @@
-export const MOCK_GEO_FEATURES = {
+export const MOCK_GEO_FEATURES_DATA = {
   type: 'FeatureCollection',
   features: [
     {
---
+++ src/data/pie.ts
@@ -0,0 +1,32 @@
+export const MOCK_PIE_DATA = [
+  {
+    id: 'hack',
+    label: 'hack',
+    value: 239,
+    color: 'hsl(104, 70%, 50%)',
+  },
+  {
+    id: 'make',
+    label: 'make',
+    value: 170,
+    color: 'hsl(162, 70%, 50%)',
+  },
+  {
+    id: 'go',
+    label: 'go',
+    value: 322,
+    color: 'hsl(291, 70%, 50%)',
+  },
+  {
+    id: 'lisp',
+    label: 'lisp',
+    value: 503,
+    color: 'hsl(229, 70%, 50%)',
+  },
+  {
+    id: 'scala',
+    label: 'scala',
+    value: 584,
+    color: 'hsl(344, 70%, 50%)',
+  },
+];
