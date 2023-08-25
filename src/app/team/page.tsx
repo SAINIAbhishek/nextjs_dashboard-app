62ef59fcc9b4f296b874004d0698e69e046fe254
Abhishek SAINI <saini.abhishek@hotmail.com>
2023-08-25 13:40:09 +0200

    feat: team page

---
+++ src/app/team/page.tsx
@@ -0,0 +1,111 @@
+'use client';
+
+import { Box, Typography, useTheme } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
+import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
+import SecurityOutlinedIcon from '@mui/icons-material/SecurityOutlined';
+import { GridColDef } from '@mui/x-data-grid/models/colDef/gridColDef';
+import { tokens } from '@/styles/theme';
+import Header from '@/components/header';
+import { MOCK_TEAM_DATA } from '@/data/team';
+
+export default function Team() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID' },
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 1,
+      cellClassName: 'name-column--cell',
+    },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      headerAlign: 'left',
+      align: 'left',
+    },
+    {
+      field: 'phone',
+      headerName: 'Phone Number',
+      flex: 1,
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      flex: 1,
+    },
+    {
+      field: 'accessLevel',
+      headerName: 'Access Level',
+      flex: 1,
+      renderCell: ({ row: { access } }) => {
+        return (
+          <Box
+            width="60%"
+            m="0 auto"
+            p="5px"
+            justifyContent="center"
+            borderRadius="4px"
+            sx={{
+              backgroundColor:
+                access === 'admin'
+                  ? colors.greenAccent[600]
+                  : access === 'manager'
+                  ? colors.greenAccent[700]
+                  : colors.greenAccent[700],
+            }}
+            display="flex">
+            {access === 'admin' && <AdminPanelSettingsOutlinedIcon />}
+            {access === 'manager' && <SecurityOutlinedIcon />}
+            {access === 'user' && <LockOpenOutlinedIcon />}
+            <Typography color={colors.grey[100]} sx={{ ml: '5px' }}>
+              {access}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+  ];
+
+  return (
+    <Box m="20px">
+      <Header title="TEAM" subtitle="Managing the Team Members" />
+
+      <Box
+        m="40px 0 0 0"
+        height="500px"
+        sx={{
+          '& .MuiDataGrid-root': {
+            border: 'none',
+          },
+          '& .MuiDataGrid-cell': {
+            borderBottom: 'none',
+          },
+          '& .name-column--cell': {
+            color: colors.greenAccent[300],
+          },
+          '& .MuiDataGrid-columnHeaders': {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: 'none',
+          },
+          '& .MuiDataGrid-virtualScroller': {
+            backgroundColor: colors.primary[400],
+          },
+          '& .MuiDataGrid-footerContainer': {
+            borderTop: 'none',
+            backgroundColor: colors.blueAccent[700],
+          },
+          '& .MuiCheckbox-root': {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}>
+        <DataGrid checkboxSelection rows={MOCK_TEAM_DATA} columns={columns} />
+      </Box>
+    </Box>
+  );
+}
